51ab5af36610dd4afa5947ec8d37982db24f9667
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-02-05 10:17:50 -0600

    Refactor i18n implementation and update Services component

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,6 @@
+import enTranslations from "./en.json";
+import esTranslations from "./es.json";
+
 export const languages = {
   en: "English",
   es: "Español",
@@ -6,38 +9,6 @@ export const languages = {
 export const defaultLang = "es";
 
 export const ui = {
-  en: {
-    "welcome": "Welcome",
-    "nav.home": "Home",
-    "nav.aboutUs": "About us",
-    "nav.services": "Services",
-    "nav.features": "Features",
-    "nav.getStarted": "Get started",
-    "hero.title-part1": "Create your custom ",
-    "hero.title-part2": "Software ",
-    "hero.title-part3": "with us.",
-    "hero.description": "We are a team of professionals who are passionate about software development. We provide full-cycle software development services. From the idea to the product launch, we are here to help you build your software.",
-    "byNumber.createdProjects": "Created projects",
-    "byNumber.happyCustomers": "Happy customers",
-    "byNumber.yearsInBusiness": "Years in business",
-    "byNumber.teamMembers": "Team members",
-    "brands.trustedBy": "Trusted by companies like",
-  },
-  es: {
-    "welcome": "Bienvenid@",
-    "nav.home": "Inicio",
-    "nav.aboutUs": "Acerca",
-    "nav.services": "Servicios",
-    "nav.features": "Características",
-    "nav.getStarted": "Comenzar",
-    "hero.title-part1": "Crea tu ",
-    "hero.title-part2": "Software ",
-    "hero.title-part3": "personalizado con nosotros.",
-    "hero.description": "Somos un equipo de profesionales apasionados por el desarrollo de software. Ofrecemos servicios de desarrollo de software de ciclo completo. Desde la idea hasta el lanzamiento del producto, estamos aquí para ayudarte a construir tu software.",
-    "byNumber.createdProjects": "Proyectos creados",
-    "byNumber.happyCustomers": "Clientes felices",
-    "byNumber.yearsInBusiness": "Años en el negocio",
-    "byNumber.teamMembers": "Miembros del equipo",
-    "brands.trustedBy": "Confiado por empresas como",
-  },
+  en: enTranslations,
+  es: esTranslations,
 } as const;
