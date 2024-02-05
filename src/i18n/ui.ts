6f8559b1c7743a5c7633b6e7c65d799b4d7252f9
Santiago <santiago.gonzalez.siordia@gmail.com>
2024-02-04 23:37:25 -0600

    Add new files and update dependencies

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "welcome": "Welcome",
+    "nav.home": "Home",
+    "nav.aboutUs": "About us",
+    "nav.services": "Services",
+    "nav.features": "Features",
+    "nav.getStarted": "Get started",
+    "hero.title-part1": "Create your custom ",
+    "hero.title-part2": "Software ",
+    "hero.title-part3": "with us.",
+    "hero.description": "We are a team of professionals who are passionate about software development. We provide full-cycle software development services. From the idea to the product launch, we are here to help you build your software."
+  },
+  es: {
+    "welcome": "Bienvenid@",
+    "nav.home": "Inicio",
+    "nav.aboutUs": "Acerca",
+    "nav.services": "Servicios",
+    "nav.features": "Características",
+    "nav.getStarted": "Comenzar",
+    "hero.title-part1": "Crea tu ",
+    "hero.title-part2": "Software ",
+    "hero.title-part3": "personalizado con nosotros.",
+    "hero.description": "Somos un equipo de profesionales apasionados por el desarrollo de software. Ofrecemos servicios de desarrollo de software de ciclo completo. Desde la idea hasta el lanzamiento del producto, estamos aquí para ayudarte a construir tu software."
+  },
+} as const;
